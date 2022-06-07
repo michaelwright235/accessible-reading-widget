74288d80119ccf6cd52458fea0bc82517c85c673
michaelwright235 <maiklpolkovnikov@gmail.com>
2022-06-08 01:43:29 +0300

    Первая версия

---
+++ accessible-reading-widget/accessible-reading-widget.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: Accessible Reading Widget
+Description: Виджет, позволяющий включить доступное чтение сайта для слабовидящих людей по ГОСТ Р 52872-2007.
+Author: Michael Wright
+Author URI: https://github.com/michaelwright235
+Version: 1.0
+License: GPL v2
+Text Domain: accessible-reading-widget
+*/
+
+/* Braille font by Yuri Kanischev, GPL License */
+	
+class AccessibleReading_widget extends WP_Widget {
+	
+	public function enqueue() {
+		wp_enqueue_style( 'arw-style', plugins_url('style.css', __FILE__) );
+		wp_enqueue_script( 'arw-script', plugins_url('accessibleReading.js', __FILE__) );
+	}
+	
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$show_icon = $instance['show_icon'];
+		
+		echo $args['before_widget'];
+		?>
+		<div class="arw_widget_wrapper arw_widget_enable_button">
+			<?php if($show_icon === 'on') {?>
+			<span class="dashicons dashicons-visibility"></span>
+			<?php } ?>
+			<a href="#"><?php echo $title; ?></a>
+		</div>
+
+		<?php
+		echo $args['after_widget'];
+	}
+	
+	public function form( $instance ) {
+		
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+		$show_icon = ! empty( $instance['show_icon'] ) ? $instance['show_icon'] : '';
+		$show_icon_checked = '';
+		if($show_icon === 'on') $show_icon_checked = 'checked';
+		?>
+
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>">Название</label>
+			<input type="text"
+				id="<?php echo $this->get_field_id( 'title' ); ?>"
+				name="<?php echo $this->get_field_name( 'title' ); ?>"
+				value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<p>
+		<input type="checkbox"
+			id="<?php echo $this->get_field_id( 'show_icon' ); ?>"
+			name="<?php echo $this->get_field_name( 'show_icon' ); ?>"
+			<?php echo $show_icon_checked; ?>/>
+			<label for="<?php echo $this->get_field_id( 'show_icon' ); ?>">Показать значок</label>
+		</p>
+		<?php 
+	}
+	
+	public function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+
+		if($new_instance[ 'show_icon' ] === 'on')
+			$instance[ 'show_icon' ] = 'on';
+		else
+			$instance[ 'show_icon' ] = '';
+		return $instance;
+	}
+
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+		
+		$widget_options = array( 
+			  'classname' => 'accessible_reading_widget',
+			  'description' => 'Accessible Reading Widget',
+		);
+		parent::__construct( 'accessible_reading_widget', 'Accessible Reading Widget', $widget_options );
+	}
+
+}
+
+add_action( 'widgets_init', function() {
+	register_widget( 'AccessibleReading_widget' );
+});
---
+++ accessible-reading-widget/accessibleReading.js
@@ -0,0 +1,222 @@
+document.accessibleReading = {
+    PARAMS: {
+        SIZE: 'fontSize',
+        COLORSCHEME: 'colorScheme',
+        IMGHIDE: 'hideImages',
+        LETTERSPACE: 'letterSpace',
+        LINEHEIGHT: 'lineHeight',
+        FONTFAMILY: 'fontFamily',
+    },
+    COOKIENAME: 'arw-settings',
+
+    enable: function(options) {
+        // Параметры по-умолчанию
+        let defaultParams = {};
+        defaultParams[this.PARAMS.COLORSCHEME] = '1';
+
+        // Класс accessible_reading включает нужные свойства
+        document.querySelector("html").classList.add("accessibleReading");
+
+        // Применяем заданные опции, либо опции по-умолчанию
+        if(Object.keys(options).length != 0)
+            this.applyParams(options);
+        else
+            this.applyParams(defaultParams);
+
+        // Добавляем таблицу для регулировки
+        document.body.prepend( Object.assign( document.createElement("div"), {
+            id: "accessibleReadingControls",
+            innerHTML: this.settingsHtml}));
+
+        let accessibleReadingControls = document.querySelector("#accessibleReadingControls");
+
+        // Каждая ссылка имеет в себе настраивающийся параметр, который считываем
+        accessibleReadingControls.querySelectorAll("a").forEach((node) => {
+            node.addEventListener("click", (event) => {
+                event.preventDefault();
+                let el = event.currentTarget;
+                let params = {};
+                // Находим измененный параметр
+                for(let p of Object.values(this.PARAMS)) {
+                    let attr = el.getAttribute('arw-'+p);
+                    if (attr) params[p] = attr;
+                };
+                this.applyParams(params);
+            });
+        });
+
+        // Действие кнопки закрытия
+        document.querySelector("#arw_widget_disable_button").
+            addEventListener("click", (event) => {
+                event.preventDefault();
+                this.disable();
+            });
+    },
+
+    bind: function (element, options = {}) {
+        element.addEventListener("click", (event) => {
+            event.preventDefault();
+            this.enable(options);
+        });
+    },
+
+    disable: function () {
+        document.querySelector("html").classList.remove("accessibleReading");
+        document.querySelector("#accessibleReadingControls").remove();
+        this.removeCookie(this.COOKIENAME);
+    },
+
+    applyParams: function (params) {
+        // Установка cookie
+        for(let [param, value] of Object.entries(params)) {
+            let cookie = this.getCookie(this.COOKIENAME);
+            let newSettings = cookie ? JSON.parse(cookie) : {};
+            newSettings[param] = value;
+            this.setCookie(this.COOKIENAME, JSON.stringify(newSettings));
+        };
+
+        let htmlClasses = document.querySelector("html").classList; // Классы html для модификации
+
+        // Размер шрифта
+        if (params[this.PARAMS.SIZE]) {
+            htmlClasses.remove("fontSize16", "fontSize20", "fontSize24", "fontSize28");
+            htmlClasses.add("fontSize"+params[this.PARAMS.SIZE]);
+        }
+
+        // Цветовая схема
+        if(params[this.PARAMS.COLORSCHEME]) {
+            htmlClasses.remove(
+                "colorScheme1", "colorScheme2", "colorScheme3", "colorScheme4", "colorScheme5");
+                htmlClasses.add("colorScheme"+params[this.PARAMS.COLORSCHEME]);
+        }
+
+        // Скрывать изображения
+        if(params[this.PARAMS.IMGHIDE]) {
+            if(params[this.PARAMS.IMGHIDE] == 'false')
+                htmlClasses.add("hiddenImages")
+            else htmlClasses.remove("hiddenImages")
+        }
+
+        // Кернинг
+        if(params[this.PARAMS.LETTERSPACE]) {
+            htmlClasses.remove("letterSpaceInitial", "letterSpace2", "letterSpace4");
+            htmlClasses.add("letterSpace"+params[this.PARAMS.LETTERSPACE]);
+        }
+
+        // Межстрочный интервал
+        if(params[this.PARAMS.LINEHEIGHT]) {
+            htmlClasses.remove("lineHeightNormal", "lineHeight2", "lineHeight4");
+            htmlClasses.add("lineHeight"+params[this.PARAMS.LINEHEIGHT]);
+        }
+
+        // Шрифт
+        if(params[this.PARAMS.FONTFAMILY]) {
+            htmlClasses.remove("fontFamilyArial", "fontFamilyTimes", "fontFamilyBraille");
+            htmlClasses.add("fontFamily"+params[this.PARAMS.FONTFAMILY]);
+        }
+    },
+    setCookie: function(name, value, options = {}) {
+        options = {
+            path: '/',
+            ...options
+        };
+    
+        if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString();
+        }
+    
+        let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+    
+        for (let optionKey in options) {
+            updatedCookie += "; " + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) {
+                updatedCookie += "=" + optionValue;
+            }
+        }
+        document.cookie = updatedCookie;
+    },
+    getCookie: function(name) {
+        let matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+          ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    },
+    removeCookie: function(name) {
+        if(!(name instanceof Array)) name = [name];
+        name.forEach((name) => {
+            this.setCookie(name, "", {
+                'max-age': -1
+            })
+        });
+    },
+    settingsHtml: `
+<div class="paramsFlex">
+    <div class="param">
+        <div class="paramName">Размер шрифта</div>
+        <div class="paramValue">
+            <a href="#" arw-fontSize="16" style="font-size: 16px">A</a>
+            <a href="#" arw-fontSize="20" style="font-size: 20px">A</a>
+            <a href="#" arw-fontSize="24" style="font-size: 24px">A</a>
+            <a href="#" arw-fontSize="28" style="font-size: 28px">A</a>
+        </div>
+    </div>
+    <div class="param">
+        <div class="paramName">Цветовая схема</div>
+        <div class="paramValue colorSchemes">
+            <a href="#" arw-colorScheme="1" style="background: #ffffff; color: #000000;">A</a>
+            <a href="#" arw-colorScheme="2" style="background: #000000; color: #ffffff;">A</a>
+            <a href="#" arw-colorScheme="3" style="background: #9DD1FF; color: #063462;">A</a>
+            <a href="#" arw-colorScheme="4" style="background: #f7f3d6; color: #4d4b43;">A</a>
+            <a href="#" arw-colorScheme="5" style="background: #3b2716; color: #a9e44d;">A</a>
+        </div>
+    </div>
+    <div class="param">
+        <div class="paramName">Изображения</div>
+        <div class="paramValue">
+            <a href="#" arw-hideImages="false">Выкл.</a>|<a href="#" arw-hideImages="true">Вкл.</a>
+        </div>
+    </div>
+    <div class="param">
+        <div class="paramName">Кернинг</div>
+        <div class="paramValue">
+            <a href="#" arw-letterSpace="Initial">Маленький</a>|<a href="#" arw-letterSpace="2">Средний</a>|<a
+                href="#" arw-letterSpace="4">Большой</a>
+        </div>
+    </div>
+    <div class="param">
+        <div class="paramName">Межстрочный интервал</div>
+        <div class="paramValue">
+            <a href="#" arw-lineHeight="Normal">Маленький</a>|<a href="#" arw-lineHeight="2">Средний</a>|<a
+                href="#" arw-lineHeight="4">Большой</a>
+        </div>
+    </div>
+    <div class="param">
+        <div class="paramName">Шрифт</div>
+        <div class="paramValue">
+            <a href="#" arw-fontFamily="Arial">Без засечек</a>|<a href="#" arw-fontFamily="Times">С
+                засечками</a>|<a href="#" arw-fontFamily="Braille">Брайля</a>
+        </div>
+    </div>
+</div>
+<div id="arw_widget_disable_button">
+    <span class="dashicons dashicons-visibility"></span> <a href="#">Обычная версия</a>
+</div>
+`,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Регистрируем кнопки
+    document.querySelectorAll(".arw_widget_enable_button").forEach(function(node) {
+        document.accessibleReading.bind( node );
+    });
+
+    // Проверяем, установлен ли куки. Если да - запускаем режим
+    // с сохраненными параметрами
+    let cookie = document.accessibleReading.getCookie(
+        document.accessibleReading.COOKIENAME
+    );
+    if (cookie) {
+        document.accessibleReading.enable( JSON.parse(cookie) );
+    }
+});
