743adab2d6bd770348e35e9a93c78013be74586b
michaelwright235 <maiklpolkovnikov@gmail.com>
2022-06-08 02:12:46 +0300

    Мелкие исправления

--- accessible-reading-widget/accessible-reading-widget.php
+++ accessible-reading-widget/accessible-reading-widget.php
@@ -1,6 +1,7 @@
 <?php
 /*
 Plugin Name: Accessible Reading Widget
+Plugin URI: https://github.com/michaelwright235/accessible-reading-widget
 Description: Виджет, позволяющий включить доступное чтение сайта для слабовидящих людей по ГОСТ Р 52872-2007.
 Author: Michael Wright
 Author URI: https://github.com/michaelwright235
@@ -8,10 +9,8 @@ Version: 1.0
 License: GPL v2
 Text Domain: accessible-reading-widget
 */
-
-/* Braille font by Yuri Kanischev, GPL License */
 	
-class AccessibleReading_widget extends WP_Widget {
+class AccessibleReadingWidget extends WP_Widget {
 	
 	public function enqueue() {
 		wp_enqueue_style( 'arw-style', plugins_url('style.css', __FILE__) );
@@ -44,8 +43,8 @@ class AccessibleReading_widget extends WP_Widget {
 		?>
 
 		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>">Название</label>
-			<input type="text"
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>">Название:</label><br>
+			<input type="text" class="widefat title"
 				id="<?php echo $this->get_field_id( 'title' ); ?>"
 				name="<?php echo $this->get_field_name( 'title' ); ?>"
 				value="<?php echo esc_attr( $title ); ?>" />
@@ -76,7 +75,7 @@ class AccessibleReading_widget extends WP_Widget {
 		
 		$widget_options = array( 
 			  'classname' => 'accessible_reading_widget',
-			  'description' => 'Accessible Reading Widget',
+			  'description' => 'Доступное чтение сайта для слабовидящих.',
 		);
 		parent::__construct( 'accessible_reading_widget', 'Accessible Reading Widget', $widget_options );
 	}
@@ -84,5 +83,5 @@ class AccessibleReading_widget extends WP_Widget {
 }
 
 add_action( 'widgets_init', function() {
-	register_widget( 'AccessibleReading_widget' );
+	register_widget( 'AccessibleReadingWidget' );
 });
